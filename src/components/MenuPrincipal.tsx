a1cba5f8d85597e8b2a6a29f0fdeb0fa70b8b26a
Eldes <eldes.poliveira@senacsp.edu.br>
2022-05-24 17:01:00 -0300

    Cria interface do Menu Principal

---
+++ src/components/MenuPrincipal.tsx
@@ -0,0 +1,13 @@
+import './MenuPrincipal.css';
+
+const MenuPrincipal = function() {
+  return (
+    <nav className="menu-principal">
+      <a href="" className="menu-item">Frios</a>
+      <a href="" className="menu-item">Padaria</a>
+      <a href="" className="menu-item">Café</a>
+    </nav>
+  );
+};
+
+export default MenuPrincipal;
