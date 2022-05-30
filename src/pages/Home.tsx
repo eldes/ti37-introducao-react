65f5dd9ad2d77a0d9c941980d7aae91bc0907d4a
Eldes <eldes.poliveira@senacsp.edu.br>
2022-05-30 13:58:47 -0300

    Adiciona botão na tela Home

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,14 @@
+import Botao from '../components/Botao';
+
 const HomePage = function() {
+	const data = '17h26';
+
 	return (
-		<h1>Homepage</h1>
+		<>
+			<h1>Homepage</h1>
+			<p>{data}</p>
+			<Botao />
+		</>
 	);
 }
 
