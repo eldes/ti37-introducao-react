e63788b9c1cedfd3db369b73f925f9b086793578
Eldes <eldes.poliveira@senacsp.edu.br>
2022-05-30 16:47:59 -0300

    Adiciona campos de texto e adiciona resposta a eventos de clique no botão

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,27 @@
 import Botao from '../components/Botao';
+import CampoTexto from '../components/CampoTexto';
 
 const HomePage = function() {
-	const data = '17h26';
+	const data = '30 de maio de 2022, 17h26';
+
+	const botaoEntrarClicado = function() {
+		alert('ENTRAR');
+	};
+	
+	const botaoCadastrarClicado = function() {
+		alert('CADASTRAR');
+	};
 
 	return (
 		<>
 			<h1>Homepage</h1>
 			<p>{data}</p>
-			<Botao />
+			<div>
+				<CampoTexto etiqueta="Nome de usuário" />
+				<CampoTexto etiqueta="Senha" />
+				<Botao texto="Entrar" botaoClicado={botaoEntrarClicado} />
+				<Botao texto="Cadastrar" botaoClicado={botaoCadastrarClicado} />
+			</div>
 		</>
 	);
 }
