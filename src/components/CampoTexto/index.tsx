1fa7aead5e429948b44de61beb34c28535506107
Eldes <eldes.poliveira@senacsp.edu.br>
2022-06-02 15:30:30 -0300

    Adicionada propriedade textoAlterado no CampoTexto

--- src/components/CampoTexto/index.tsx
+++ src/components/CampoTexto/index.tsx
@@ -1,15 +1,25 @@
-import { FunctionComponent } from 'react';
+import { ChangeEventHandler, FunctionComponent } from 'react';
 import styles from './styles.module.css';
 
 type Props = {
   etiqueta: string
+  textoAlterado?: (texto: string) => void
 };
 
 const CampoTexto: FunctionComponent<Props> = function(props) {
+
+  const valorAlterado: ChangeEventHandler<HTMLInputElement> = function(evento) {
+    if (props.textoAlterado) {
+      const input = evento.target;
+      const texto = input.value;
+      props.textoAlterado(texto);
+    }
+  };
+
   return (
     <div className={styles.CampoTexto}>
       <label>{props.etiqueta}:</label>
-      <input placeholder={props.etiqueta} />
+      <input placeholder={props.etiqueta} onChange={valorAlterado} />
     </div>
   );
 };
