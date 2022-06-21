286ca41ef5308a19880f7ab90ebd7c4bebf9dbcb
Eldes <eldes.poliveira@senacsp.edu.br>
2022-06-21 14:59:09 -0300

    Carrega a lista de produtos ao exibir a tela

--- src/pages/Frios.tsx
+++ src/pages/Frios.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import ProdutoItem from '../components/ProdutoItem';
 import Produto from '../models/Produto';
 import ProdutosService from '../services/Produtos';
@@ -6,17 +6,23 @@ import ProdutosService from '../services/Produtos';
 const FriosPage = function() {
 
 	const [carregando, setCarregando] = useState<boolean>(false);
-
 	const [produtos, setProdutos] = useState<Produto[]>([]);
-
-	const botaoRecarregarClicado = function() {
+	
+	const recarregar = function () {
 		setCarregando(true);
 		ProdutosService.lerTodos(function (produtos) {
 			setProdutos(produtos);
 			setCarregando(false);
 		});
-		
 	};
+
+	const botaoRecarregarClicado = function () {
+		recarregar();
+	};
+
+	useEffect(function () {
+		recarregar();
+	}, []);
 
 	return (
 		<>
